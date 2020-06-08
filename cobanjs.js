424aa76c86d2763563689b23905bd39087c64c71
duyninhhust <63568491+duyninhhust@users.noreply.github.com>
2020-06-08 10:07:20 +0700

    Add files via upload

---
+++ cobanjs.js
@@ -0,0 +1,90 @@
+// // console.log('tadaaa');
+// // for (var i = 0; i< 100; i++) {
+// //     console.log(i);
+    
+// // }
+
+
+// // var sv = ['Viet','NAM','hUONG','Lan','Nga' ];
+// // console.log('In ra tat ca phan tu');
+// // for (let index = 0; index < sv.length; index++) {
+// //     console.log(sv[index]);
+    
+// // }
+
+
+
+// // For in
+// // var hocvien = {
+// //     ten:"Ninh",
+// //     tuoi:21,
+// //     diemToan:10,
+// //     diemToan:10,
+// //     diemToan:10,
+// //     diemToan:10,
+// //     chieuCao:   179,
+
+// // }
+
+// // console.log(hocvien.ten);
+// // var x;
+// // for(x in hocvien){
+// //     console.log(hocvien[x]);
+// // }
+
+
+// //useSTrict
+// // che do strict bat buoc co var trong khai bao bien'
+
+
+
+
+
+// var x= "iphone,android,nokia";
+// // console.log(x.length);            
+// var vitri = x.search('iphone');    indexOf = search
+// // neu co iphone trong x,thi tra ve vi tri cua no
+// // neu khong co thi tra ve -1
+// if(vitri!=-1){
+//     console.log(" tim thay iphone");
+//     var moi = x.replace('iphone','Bphone');
+//     console.log(moi);
+// }else{
+//     console.log("K tim thay");
+// }
+
+
+
+
+
+
+
+
+
+// // for (let index = 0; index < array.length; index++) {
+// //     const element = array[index];
+    
+// // }
+// // for (const key in object) {
+// //     if (object.hasOwnProperty(key)) {
+// //         const element = object[key];
+        
+// //     }
+// // }
+
+
+
+//x.split
+// var x ="Bo ,Me, Con,Emtrai1,Emtrai2";
+// var mang = x.split(',');
+// console.log(mang);
+// Bien x thanh 1 mang
+
+
+
+
+
+// Ham`x= x.toString(); Bien x tu` so thanh` Chu~.
+// var chu1 ="10"
+// Ham` chu1 = Number(chu1);  Bien chu1 thanh so 10
+// ham` parseInt() giong Number() nhung chi bien thanh so nguyen
